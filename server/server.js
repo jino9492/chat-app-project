cdcf7a33b7e375f3731be716abf9559b77faa562
정진오 <jino9492@gmail.com>
2024-03-18 03:00:20 +0900

    server update

--- server/server.js
+++ server/server.js
@@ -16,6 +16,10 @@ io.on("connection", (socket) => {
     console.log(item.name + ": " + item.message + " / " + item.date + " / " + item.timeData);
     io.emit("receive message", {name : item.name, message: item.message, date: item.date, timeData: item.timeData});
   })
+
+  socket.on("send image-file", (data) => {
+    io.emit("recieve image-file", { name: data.name, file: data.file, date: data.date, timeData: data.timeData });
+  });
 });
 
 httpServer.listen(8000);
